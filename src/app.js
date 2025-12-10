1f4b90d41178ca60e928641c1b929c5ddbd397a6
aditya07879 <aditya07879@gmail.com>
2025-12-10 22:00:57 +0530

    setup cors configuration

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
-import express from 'express'
-
+import express from 'express';
+import cors from 'cors';
 const app = express();
+
+//basic config
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// cors config
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(',')  || "http://localhost:5173",
+    credentials: true,
+    methods : ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
+
+
 
 
 app.get('/', (req, res) => {
