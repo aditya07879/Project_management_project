b92154013263d6a8c58708eee1189c1b709895eb
aditya07879 <aditya07879@gmail.com>
2025-12-10 21:28:45 +0530

    Separated index for server listening!

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import 'dotenv/config';
-import express from 'express'
-const app = express()
+import app from './app.js';
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
