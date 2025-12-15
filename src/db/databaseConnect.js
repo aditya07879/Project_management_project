2ad43fc921cb7e246c298971c2022262718c11de
aditya07879 <aditya07879@gmail.com>
2025-12-15 21:10:38 +0530

    Connected MongoDB

---
+++ src/db/databaseConnect.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+
+const ConnectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log(" ☑️ MongoDB is connected");
+        
+        
+    } catch (error) {
+        console.error("❌ MongoDB connection error",error);
+        process.exit(1);
+    }
+}
+
+
+export default ConnectDB;
